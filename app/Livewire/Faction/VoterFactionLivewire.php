359965acfac93f36727c31e1584e444d7b2dac96
ManoguidKim <manoguidk@bayambang.gov.ph>
2025-05-01 20:06:25 +0800

    Update faction view

--- app/Livewire/Faction/VoterFactionLivewire.php
+++ app/Livewire/Faction/VoterFactionLivewire.php
@@ -12,9 +12,9 @@ class VoterFactionLivewire extends Component
 
     public function render()
     {
-        $voterTaggedAlly = Voter::where(['status' => 'Active', 'remarks' => 'Ally', 'municipality_id' => auth()->user()->municipality_id])->count();
-        $voterTaggedOpponent = Voter::where(['status' => 'Active', 'remarks' => 'Opponent', 'municipality_id' => auth()->user()->municipality_id])->count();
-        $voterTaggedUndecided = Voter::where(['status' => 'Active', 'remarks' => 'Undecided', 'municipality_id' => auth()->user()->municipality_id])->count();
+        $voterTaggedAlly = Voter::where(['status' => 'Active', 'remarks' => 'Ally', 'municipality_id' => auth()->user()->municipality_id, 'is_checked' => 1])->count();
+        $voterTaggedOpponent = Voter::where(['status' => 'Active', 'remarks' => 'Opponent', 'municipality_id' => auth()->user()->municipality_id, 'is_checked' => 1])->count();
+        $voterTaggedUndecided = Voter::where(['status' => 'Active', 'remarks' => 'Undecided', 'municipality_id' => auth()->user()->municipality_id, 'is_checked' => 1])->count();
 
         $voterFactions = Barangay::select('barangays.name')
             ->selectRaw('COALESCE(SUM(voters.remarks = "ally"), 0) as ally_count')
@@ -22,6 +22,8 @@ class VoterFactionLivewire extends Component
             ->selectRaw('COALESCE(SUM(voters.remarks = "undecided"), 0) as undecided_count')
             ->leftJoin('voters', 'barangays.id', '=', 'voters.barangay_id')
             ->where('barangays.municipality_id', auth()->user()->municipality_id)
+            ->where('status', 'Active')
+            ->where('is_checked', 1)
             ->where('barangays.name', 'like', '%' . $this->search . '%')
             ->groupBy('barangays.name')
             ->orderBy('barangays.name', 'asc')
