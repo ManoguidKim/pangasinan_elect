a6e8e79fc4fb5cb8cad13a23fe1d344a7e8a2cc1
ManoguidKim <manoguidk@bayambang.gov.ph>
2025-05-03 11:22:25 +0800

    ddsdsdsd

--- resources/views/scanner/selectevent.blade.php
+++ resources/views/scanner/selectevent.blade.php
@@ -37,7 +37,7 @@
      <form action="{{ route('admin-scanner') }}" method="post">
           @csrf
           <div>
-               <select name="event" class="bg-gray-50 border border-blue-300 boder-dashed text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+               <select name="event" class="bg-gray-50 border border-blue-300 boder-dashed text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                     <option value="">Chose Event</option>
                     @foreach ($events as $event)
                     <option value="{{ $event->id }}">{{ $event->name }}</option>
